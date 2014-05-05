e4e535d963626e1de24bce8709713bfa25e2bc26
Alex <alexander,ray.boyd@gmail.com>
2014-05-04 18:22:57 -0700

    nah second commit da bes

---
+++ Assets/Code/ObjectSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectSpawner : MonoBehaviour 
+{
+	public float SpawnInterval = 2;
+	public GameObject ObjectPrefab;
+	public float SpawnRange = 10f;
+	public float SpawnVelocity = 10f;
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine(SpawnCoroutine());
+	}
+	
+	// Update is called once per frame
+	private IEnumerator SpawnCoroutine() 
+	{
+		GameObject newObj = null;
+		while(true)
+		{
+			yield return new WaitForSeconds(SpawnInterval);
+			newObj = GameObject.Instantiate(ObjectPrefab) as GameObject;
+			newObj.transform.parent = transform;
+			newObj.transform.position = Random.insideUnitSphere * SpawnRange;
+			newObj.rigidbody.AddForce(newObj.transform.position * -SpawnVelocity);
+		}
+	}
+}
---
+++ Assets/Gun.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+
+	public Transform Orientation;
+	public LayerMask lm;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyUp(KeyCode.Return))
+		{
+			Debug.DrawRay(Orientation.position, Orientation.forward * 100f, Color.red, 5f); 
+			RaycastHit hit;
+			if(Physics.Raycast(Orientation.position, Orientation.forward, out hit, 10000f, lm))
+			{
+				hit.rigidbody.AddExplosionForce(100f, hit.point, 1f);
+				Debug.Log("PEWPEW");
+			}
+		}
+	
+	}
+}
